b54d365bff579bd813861e060e6e6c89f387170b
Scotty Skidmore <scottys@eftsure.com.au>
2024-12-02 17:04:32 +1100

    day 2 part 2

--- src/aoc24/day02/index.ts
+++ src/aoc24/day02/index.ts
@@ -25,11 +25,48 @@ function isAscending(report: number[]): boolean {
 }
 
 
-
 function isSafe(report: number[]): boolean {
-    return isAscending(report) || isAscending(report.reverse());
-    
+    return isAscending(report) || isAscending([...report].reverse());
 }
+
+
+// Leaving this here as a cautionary tale for all those who thought they were 
+// a too smart for their own good rather than just brute forcing it...
+function isMosltyAscending(report: number[]): boolean {
+    let previous = report[0];
+    let badCount = 0;
+
+    // checking if the first or last can be removed
+    if (isSafe(report.slice(1)) || isSafe(report.slice(0, -1))) {
+        return true;
+    }
+
+    for (let i of report.slice(1)) {
+        if (!(previous < i && previous + 3 >= i)) { 
+            if (badCount >= 1) {
+                return false;
+            } else {
+                badCount++;
+            }
+        } else {
+            // keep previous the same if the last case was bad
+            previous = i;
+        } 
+    }
+
+    return true;
+}
+
+function isMostlySafe(report: number[]): boolean { // Could use generics here?
+    for (let i = 0; i < report.length; i++) {
+        let removeOne = [...report.slice(0, i), ...(report.slice(i + 1))];
+        if (isSafe(removeOne)) {
+            return true;
+        }
+    }
+    return false;
+}
+
 
 function part1() {
     let input = readInput();
@@ -46,7 +83,13 @@ function part1() {
 function part2() {
     let input = readInput();
     let data = processData(input);
-    return 0;
+    let safeCount = 0;
+    for (let report of data) {
+        if (isMostlySafe(report)) {
+            safeCount++;
+        }
+    }
+    return safeCount;
 }
 
 console.log(`Part 1 Answer: ${part1()}`);
